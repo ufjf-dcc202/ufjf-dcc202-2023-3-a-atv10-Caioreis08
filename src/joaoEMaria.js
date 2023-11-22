557e745bef5c5084494b129c9cf9e70ef49a07c0
Caio Reis <fernandes.caio@estudante.ufjf.br>
2023-11-22 20:55:08 -0300

    update joaoEMaria.js

--- src/joaoEMaria.js
+++ src/joaoEMaria.js
@@ -1,6 +1,24 @@
 let joao = 1;
 let maria = 2;
 
+function setJoao(valor){
+    if (valor >= 0){
+        joao = valor;
+    }
+    else{
+        joao = 0;
+    }
+}
+
+function setMaria (valor){
+    if (valor >= 0){
+        maria = valor;
+    }
+    else{
+        maria = 0;
+    }
+}
+
 function getMaria(){
     return maria;
 }
@@ -8,5 +26,14 @@ function getMaria(){
 function getJoao(){
     return joao;
 }
+function deMariaParaJoao(){
+    joao = joao + maria;
+    maria = 0;
+}
 
-export {getJoao, getMaria};
+function deJoaoParaMaria(){
+    maria = maria + joao;
+    joao = 0;
+}
+
+export {getJoao, getMaria, deMariaParaJoao, deJoaoParaMaria, setJoao, setMaria};
