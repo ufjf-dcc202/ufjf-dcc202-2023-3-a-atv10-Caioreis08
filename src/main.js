0cde5bdbd72d982ac9bb1a6e1fe3ed975b669d27
Caio Reis <fernandes.caio@estudante.ufjf.br>
2023-11-22 20:55:25 -0300

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { getMaria, getJoao } from "./joaoEMaria";
+import { getMaria, getJoao, deJoaoParaMaria, setMaria, setJoao} from "./joaoEMaria";
 
 console.log("MAIN");
+setMaria(4);
+setJoao(2);
+
 let maria = getMaria();
 let joao = getJoao();
+console.log(maria, joao);
+
+deJoaoParaMaria();
+
+maria = getMaria();
+joao = getJoao();
 console.log(maria, joao);
